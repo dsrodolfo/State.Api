e3beda1c0784f033afeac0c581a965fe4099b7c7
Rodolfo Dias da Silva <rodolfod91@hotmail.com>
2021-11-03 22:24:54 -0300

    feat: Add Dependency Injection.

--- State.Api/Program.cs
+++ State.Api/Program.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
 using State.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<StateService>();
 
 var app = builder.Build();
 
@@ -15,7 +17,15 @@ if (app.Environment.IsDevelopment())
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/State/getAll", () => new StateService().GetAllStates());
-app.MapGet("/State/getAll/{name}", (string name) => new StateService().GetAllStatesByName(name));
+app.MapGet("/State/getAll", ([FromServices] StateService stateService) =>
+{
+    return stateService.GetAllStates();
+});
+
+app.MapGet("/State/getAll/{name}", ([FromServices] StateService stateService, string name) =>
+{
+    string[] states = stateService.GetAllStatesByName(name);
+    return states.Length != 0 ? Results.Ok(states) : Results.NotFound();
+});
 
 app.Run();
